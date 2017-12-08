4af87267ebc3567d6869a44cb0e8beb09fff630d
Louis Vanier <louis.vanier@shopify.com>
2017-12-08 11:20:10 -0500

    inject parsed component

--- packages/slate-component-loader/index.js
+++ packages/slate-component-loader/index.js
@@ -2,28 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const parse = require('./parser');
 
-function findComponentDeclarations(source) {
-  const regex = /\{%\s*component '(.*?)',?\s?(.*?)%\}/g;
-  const matches = [];
-  let match;
-
-  while ((match = regex.exec(source)) !== null) {
-    // This is necessary to avoid infinite loops with zero-width matches
-    if (match.index === regex.lastIndex) {
-      regex.lastIndex++;
-    }
-
-    if (match[1]) {
-      matches.push({
-        fullMatch: match[0],
-        componentName: match[1],
-        getComponentProperties(match[2]),
-      });
-    }
-  }
-
-  return matches;
-}
+const regex = /\{%\s*component '(.*?)',?\s?(.*?)%\}/g
 
 function getComponentProperties(propertiesString) {
   if (!!propertiesString) {
@@ -41,33 +20,32 @@ function getComponentProperties(propertiesString) {
   });
 }
 
-module.exports = function(content) {
-  this.cacheable();
-
-  const components = findComponentDeclarations(content);
-
-  if (components.length <= 0) {
-    return content;
-  }
-
-  components.forEach(component => {
-    const componentName = component.componentName;
-    const componentPath = path.resolve(
+function componentInjector(match, componentName, attributes, offset, string) {
+  const componentPath = path.resolve(
       `src/components/${componentName}.component.liquid`
-    );
-
-    const componentContent = fs.readFileSync(componentPath, 'utf-8');
-
-    const parts = parse(
+  );
+  const componentContent = fs.readFileSync(componentPath, 'utf-8');
+  const parts = parse(
       componentContent,
       componentName,
       this.sourceMap,
       componentPath,
       false
-    );
+  );
 
-    console.log(parts);
-  });
+  //TODO => also inject the {% assign %} for each liquid attribute
+  //   foreach attribute, do the following
+  //   1. assign old_var the current variable
+  //   2. assign the value to var
+  //   3. render the components
+  //   4. assign var the value of old_var
+  console.log(parts)
 
-  return content;
+  return parts;
+}
+
+module.exports = function(content) {
+  this.cacheable();
+
+  return content.replace(regex, componentInjector);
 };
