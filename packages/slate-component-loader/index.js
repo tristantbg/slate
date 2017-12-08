41001024e1cf346bfe74755b44be369e084a94c4
t-kelly <thomas.kelly@shopify.com>
2017-12-08 14:07:29 -0500

    Spit out the extracted liquid content with custom assigns

--- packages/slate-component-loader/index.js
+++ packages/slate-component-loader/index.js
@@ -2,48 +2,54 @@ const fs = require('fs');
 const path = require('path');
 const parse = require('./parser');
 
-const regex = /\{%\s*component '(.*?)',?\s?(.*?)%\}/g
+const regex = /\{%\s*component '(.*?)',?\s?(.*?)%\}/g;
 
 function getComponentProperties(propertiesString) {
-  if (!!!propertiesString) {
+  if (!propertiesString) {
     return [];
   }
 
   return propertiesString.split(',').map(keyValue => {
     return {
       key: keyValue.split(':')[0].trim(),
-      value: keyValue
-        .split(':')[1]
-        .trim(),
+      value: keyValue.split(':')[1].trim(),
     };
   });
 }
 
 function componentInjector(match, componentName, properties, offset, string) {
   const componentPath = path.resolve(
-      `src/components/${componentName}.component.liquid`
+    `src/components/${componentName}.component.liquid`
   );
   const componentContent = fs.readFileSync(componentPath, 'utf-8');
+  let content = '';
   const parts = parse(
-      componentContent,
-      componentName,
-      this.sourceMap,
-      componentPath,
-      false
+    componentContent,
+    componentName,
+    this.sourceMap,
+    componentPath,
+    false
   );
 
   parts.assignProperties = '';
   parts.restoreProperties = '';
   getComponentProperties(properties).map(keyValuePair => {
     const tempVariableName = `${new Date().getTime()}_${keyValuePair.key}`;
-    parts.assignProperties += `{% assign ${tempVariableName} = ${keyValuePair.key} %}\n` +
-    `{% assign ${keyValuePair.key} = ${keyValuePair.value} %}\n`
-    parts.restoreProperties += `{% assign ${keyValuePair.key} = ${tempVariableName} %}\n`
-
+    parts.assignProperties += `{% assign ${tempVariableName} = ${
+      keyValuePair.key
+    } %}\n
+    {% assign ${keyValuePair.key} = ${keyValuePair.value} %}\n`;
+    parts.restoreProperties += `{% assign ${keyValuePair.key} = ${
+      tempVariableName
+    } %}\n`;
   });
 
+  content += parts.assignProperties;
+  content += parts.template.content;
+  content += parts.restoreProperties;
+
   console.log(parts);
-  return parts;
+  return content;
 }
 
 module.exports = function(content) {
